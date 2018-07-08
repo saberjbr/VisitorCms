821e553b9f4ae17fcbc5b256ffe5c82c9e3cbda6
saberjbr <saberjbr@gmail.com>
2018-07-08 13:11:31 +0430

    Dashboard Controller Completed !!!!

--- VisitoringCMS/Controllers/DashboardController.cs
+++ VisitoringCMS/Controllers/DashboardController.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VisitoringCMS.Models;
 
 namespace VisitoringCMS.Controllers
 {
     public class DashboardController : Controller
     {
+        VisitoringEntities db = new VisitoringEntities();
         // GET: Dashboard
         public ActionResult Index()
         {
@@ -19,19 +22,34 @@ namespace VisitoringCMS.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult CreateVisitor(string str)
+        public ActionResult CreateVisitor(string name_family, string user_name, string email, string password, HttpPostedFileBase photo)
         {
-            return View();
+            /*  var category = new Category();
+              category.Name = name;
+              category.Photo = Convert.ToBase64String(new BinaryReader(photo.InputStream).ReadBytes(photo.ContentLength));
+              db.Categories.Add(category);
+              db.SaveChanges();*/
+            var visitor = new Visitor();
+            visitor.Name = name_family;
+            visitor.Username = user_name;
+            visitor.Email = email;
+            visitor.Password = password;
+            visitor.Picture = Convert.ToString(new BinaryReader(photo.InputStream).ReadBytes(photo.ContentLength));
+            db.Visitor.Add(visitor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        public ActionResult CreateAgent()
+            public ActionResult CreateAgent()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult CreateAgent(string str)
+        public ActionResult CreateAgent(string name, string address, string lat, string lng)
         {
-            return null;
+            db.agent.Add(new agent { Name = name, Address = address, Lat = Convert.ToDouble(lat), Lng = Convert.ToDouble(lng) });
+            db.SaveChanges();
+            return View();
         }
 
         public ActionResult CreateProduct()
@@ -39,9 +57,27 @@ namespace VisitoringCMS.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult CreateProduct(string str)
+        public ActionResult CreateProduct(string name, int price, int sale_price, string desc, string stock, HttpPostedFileBase photo)
         {
-            return View();
+            var product = new Product();
+            product.Name = name;
+            product.Price = Convert.ToDecimal(price);
+            product.Sale_Price = Convert.ToDecimal(sale_price);
+            product.Description = desc;
+            if (!String.IsNullOrEmpty(stock))
+            {
+                product.In_stock = true;
+            }
+            else
+            {
+                product.In_stock = false;
+
+            }
+
+            product.Picture = Convert.ToString(new System.IO.BinaryReader(photo.InputStream).ReadBytes(photo.ContentLength));
+            db.Product.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
